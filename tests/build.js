e0c170888c785680274f313aa67328f523db16bb
DarkPark <darkpark@pisem.net>
2015-12-16 22:38:39 +0200

    prepare for publishing

--- tests/build.js
+++ tests/build.js
@@ -48,66 +48,66 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	/**
-	 * @author Stanislav Kalashnik <sk@infomir.eu>
+	 * @author Stanislav Kalashnik <darkpark.main@gmail.com>
 	 * @license GNU GENERAL PUBLIC LICENSE Version 3
 	 */
-	
+
 	'use strict';
-	
+
 	/* jshint undef:false */
-	
+
 	// dependencies
 	var Model = __webpack_require__(/*! ../index */ 1);
-	
-	
+
+
 	// declare named module
 	QUnit.module('model');
-	
-	
+
+
 	test('constructor', function testConstructor () {
 		var m;
-	
+
 		m = new Model();
 		strictEqual(typeof m.data, 'object', 'type');
 		strictEqual(typeof m.events, 'object', 'type');
 		strictEqual(m.data.constructor, Object, 'constructor type');
 		strictEqual(Object.keys(m.data).length, 0, 'keys');
-	
+
 		m = new Model(null);
 		strictEqual(m.data.constructor, Object, 'constructor type');
 		strictEqual(Object.keys(m.data).length, 0, 'keys');
-	
+
 		m = new Model(undefined);
 		strictEqual(m.data.constructor, Object, 'constructor type');
 		strictEqual(Object.keys(m.data).length, 0, 'keys');
-	
+
 		m = new Model(123);
 		strictEqual(m.data.constructor, Object, 'constructor type');
 		strictEqual(Object.keys(m.data).length, 0, 'keys');
-	
+
 		m = new Model('qwe');
 		strictEqual(m.data.constructor, Object, 'constructor type');
 		strictEqual(Object.keys(m.data).length, 0, 'keys');
-	
+
 		m = new Model(true);
 		strictEqual(m.data.constructor, Object, 'constructor type');
 		strictEqual(Object.keys(m.data).length, 0, 'keys');
-	
+
 		m = new Model([]);
 		strictEqual(m.data.constructor, Array, 'constructor type');
 		strictEqual(Object.keys(m.data).length, 0, 'keys');
-	
+
 		m = new Model({a:1});
 		strictEqual(m.data.constructor, Object, 'constructor type');
 		strictEqual(Object.keys(m.data).length, 1, 'keys');
 	});
-	
-	
+
+
 	test('data', function testData () {
 		var m;
-	
+
 		expect(16);
-	
+
 		m = new Model();
 		strictEqual(m.init(), false, 'wrong param');
 		strictEqual(m.init(''), false, 'wrong param');
@@ -120,12 +120,12 @@
 		strictEqual(m.init(false), false, 'wrong param');
 		strictEqual(m.init(1,1,1,1), false, 'wrong param');
 		strictEqual(m.init([]), true, 'wrong array param');
-	
+
 		strictEqual(m.init({}), true, 'normal param');
-	
+
 		m.init({a:1,b:2});
 		propEqual(m.data, {a:1,b:2}, 'normal param');
-	
+
 		m.addListener('init', function ( event ) {
 			propEqual(event.data, {a:123}, 'init event');
 		});
@@ -136,16 +136,16 @@
 		m.init();
 		m.init({a:123});
 	});
-	
-	
+
+
 	test('clear', function testClear () {
 		var m;
-	
+
 		expect(5);
-	
+
 		m = new Model({a:1,b:2});
 		propEqual(m.data, {a:1,b:2}, 'normal init');
-	
+
 		m.addListener('clear', function ( event ) {
 			propEqual(event.data, {a:1,b:2}, 'clear event');
 			propEqual(m.data, {}, 'clear event check');
@@ -153,14 +153,14 @@
 		ok(m.clear(), 'do clear');
 		ok(!m.clear(), 'do clear again');
 	});
-	
-	
+
+
 	test('has', function testHas () {
 		var m;
-	
+
 		m = new Model({a:1,b:2});
 		propEqual(m.data, {a:1,b:2}, 'normal init');
-	
+
 		ok(m.has('a'), 'present');
 		ok(m.has('b'), 'present');
 		ok(!m.has('c'), 'not present');
@@ -173,18 +173,18 @@
 		ok(!m.has(true), 'not present');
 		ok(!m.has([]), 'not present');
 		ok(!m.has({}), 'not present');
-	
+
 		m.clear();
 		ok(!m.has('a'), 'not present');
 	});
-	
-	
+
+
 	test('get', function testGet () {
 		var m;
-	
+
 		m = new Model({a:1,b:2});
 		propEqual(m.data, {a:1,b:2}, 'normal init');
-	
+
 		strictEqual(m.get('a'), 1, 'one attr');
 		strictEqual(m.get('c'), undefined, 'missing attr');
 		strictEqual(m.get(123), undefined, 'missing attr');
@@ -195,13 +195,13 @@
 		strictEqual(m.get({}), undefined, 'missing attr');
 		strictEqual(m.get([]), undefined, 'missing attr');
 	});
-	
-	
+
+
 	test('set', function testSet () {
 		var m;
-	
+
 		expect(14);
-	
+
 		m = new Model();
 		m.addListener('change', function ( event ) {
 			strictEqual(event.name, 'a', 'create: name');
@@ -211,7 +211,7 @@
 		});
 		ok(m.set('a', 123), 'set operation');
 		propEqual(m.data, {a:123}, 'check');
-	
+
 		m = new Model({a:0});
 		m.addListener('change', function ( event ) {
 			strictEqual(event.name, 'a', 'update: name');
@@ -221,7 +221,7 @@
 		});
 		ok(m.set('a', 222), 'set operation');
 		propEqual(m.data, {a:222}, 'check');
-	
+
 		m = new Model({a:0});
 		m.addListener('change', function () {
 			ok(false, 'should not be called');
@@ -229,23 +229,23 @@
 		ok(!m.set('a', 0), 'set operation');
 		propEqual(m.data, {a:0}, 'check');
 	});
-	
-	
+
+
 	test('unset', function testUnset () {
 		var m;
-	
+
 		expect(11);
-	
+
 		m = new Model();
 		propEqual(m.data, {}, 'normal init');
 		m.addListener('change', function () {
 			ok(false, 'should not be called');
 		});
 		ok(!m.unset('qwe'), 'nothing to remove');
-	
+
 		m = new Model({a:1,b:2});
 		propEqual(m.data, {a:1,b:2}, 'normal init');
-	
+
 		m.addListener('change', function ( event ) {
 			strictEqual(event.name, 'a', 'removal: name');
 			strictEqual('prev' in event, true, 'removal: prev');
@@ -269,15 +269,15 @@
 	/* WEBPACK VAR INJECTION */(function(__filename) {/**
 	 * @module stb-model
 	 *
-	 * @author Stanislav Kalashnik <sk@infomir.eu>
+	 * @author Stanislav Kalashnik <darkpark.main@gmail.com>
 	 * @license GNU GENERAL PUBLIC LICENSE Version 3
 	 */
-	
+
 	'use strict';
-	
+
 	var Emitter = __webpack_require__(/*! stb-emitter */ 2);
-	
-	
+
+
 	/**
 	 * Base model implementation.
 	 *
@@ -295,10 +295,10 @@
 			if ( typeof this !== 'object' ) { throw new Error(__filename + ': must be constructed via new'); }
 			if ( data && typeof data !== 'object' ) { throw new Error(__filename + ': wrong data type'); }
 		}
-	
+
 		// parent constructor call
 		Emitter.call(this);
-	
+
 		/**
 		 * Model attributes with given data or empty hash table.
 		 *
@@ -306,13 +306,13 @@
 		 **/
 		this.data = data || {};
 	}
-	
-	
+
+
 	// inheritance
 	Model.prototype = Object.create(Emitter.prototype);
 	Model.prototype.constructor = Model;
-	
-	
+
+
 	/**
 	 * Remove all attributes from the model event.
 	 *
@@ -321,8 +321,8 @@
 	 * @type {Object}
 	 * @property {Object} data old model attributes
 	 */
-	
-	
+
+
 	/**
 	 * Remove all attributes from the model.
 	 *
@@ -332,30 +332,30 @@
 	 */
 	Model.prototype.clear = function () {
 		var data = this.data;
-	
+
 		if ( true ) {
 			if ( typeof data !== 'object' ) { throw new Error(__filename + ': wrong data type'); }
 		}
-	
+
 		// is there any data?
 		if ( Object.keys(data).length > 0 ) {
 			// reset
 			this.data = {};
-	
+
 			// there are some listeners
 			if ( this.events['clear'] ) {
 				// notify listeners
 				this.emit('clear', {data: data});
 			}
-	
+
 			return true;
 		}
-	
+
 		// nothing was done
 		return false;
 	};
-	
-	
+
+
 	/**
 	 * Set model data event.
 	 *
@@ -364,8 +364,8 @@
 	 * @type {Object}
 	 * @property {Object} data new model attributes
 	 */
-	
-	
+
+
 	/**
 	 * Clear and set model data.
 	 *
@@ -379,29 +379,29 @@
 		if ( true ) {
 			if ( typeof data !== 'object' ) { throw new Error(__filename + ': wrong data type'); }
 		}
-	
+
 		// valid input
 		if ( data ) {
 			// reset data
 			this.clear();
-	
+
 			// init with given data
 			this.data = data;
-	
+
 			// there are some listeners
 			if ( this.events['init'] ) {
 				// notify listeners
 				this.emit('init', {data: data});
 			}
-	
+
 			return true;
 		}
-	
+
 		// nothing was done
 		return false;
 	};
-	
-	
+
+
 	/**
 	 * Check an attribute existence.
 	 *
@@ -413,12 +413,12 @@
 		if ( true ) {
 			if ( typeof this.data !== 'object' ) { throw new Error(__filename + ': wrong this.data type'); }
 		}
-	
+
 		// hasOwnProperty method is not available directly in case of Object.create(null)
 		//return Object.hasOwnProperty.call(this.data, name);
 		return this.data.hasOwnProperty(name);
 	};
-	
+
 	/**
 	 * Get a model attribute value by name.
 	 *
@@ -430,11 +430,11 @@
 		if ( true ) {
 			if ( typeof this.data !== 'object' ) { throw new Error(__filename + ': wrong this.data type'); }
 		}
-	
+
 		return this.data[name];
 	};
-	
-	
+
+
 	/**
 	 * Update or create a model attribute event.
 	 *
@@ -445,8 +445,8 @@
 	 * @property {*} [prev] old/previous attribute value (can be absent on attribute creation)
 	 * @property {*} [curr] new/current attribute value (can be absent on attribute removal)
 	 */
-	
-	
+
+
 	/**
 	 * Update or create a model attribute.
 	 *
@@ -459,44 +459,44 @@
 	Model.prototype.set = function ( name, value ) {
 		var isAttrSet = name in this.data,
 			emitData  = {name: name, curr: value};
-	
+
 		if ( true ) {
 			if ( typeof this.data !== 'object' ) { throw new Error(__filename + ': wrong this.data type'); }
 		}
-	
+
 		if ( isAttrSet ) {
 			// update
 			emitData.prev = this.data[name];
 			// only if values are different
 			if ( value !== emitData.prev ) {
 				this.data[name] = value;
-	
+
 				// there are some listeners
 				if ( this.events['change'] ) {
 					// notify listeners
 					this.emit('change', emitData);
 				}
-	
+
 				return true;
 			}
 		} else {
 			// create
 			this.data[name] = value;
-	
+
 			// there are some listeners
 			if ( this.events['change'] ) {
 				// notify listeners
 				this.emit('change', emitData);
 			}
-	
+
 			return true;
 		}
-	
+
 		// nothing was done
 		return false;
 	};
-	
-	
+
+
 	/**
 	 * Delete the given attribute by name.
 	 *
@@ -508,38 +508,38 @@
 	Model.prototype.unset = function ( name ) {
 		var isAttrSet = name in this.data,
 			emitData;
-	
+
 		if ( true ) {
 			if ( typeof this.data !== 'object' ) { throw new Error(__filename + ': wrong this.data type'); }
 		}
-	
+
 		if ( isAttrSet ) {
 			emitData = {name: name, prev: this.data[name]};
 			delete this.data[name];
-	
+
 			// there are some listeners
 			if ( this.events['change'] ) {
 				// notify listeners
 				this.emit('change', emitData);
 			}
-	
+
 			return true;
 		}
-	
+
 		// nothing was done
 		return false;
 	};
-	
-	
+
+
 	if ( true ) {
 		// expose to the global scope
 		window.Model = Model;
 	}
-	
-	
+
+
 	// public
 	module.exports = Model;
-	
+
 	/* WEBPACK VAR INJECTION */}.call(exports, "/index.js"))
 
 /***/ },
@@ -550,13 +550,13 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(__filename) {/**
-	 * @author Stanislav Kalashnik <sk@infomir.eu>
+	 * @author Stanislav Kalashnik <darkpark.main@gmail.com>
 	 * @license GNU GENERAL PUBLIC LICENSE Version 3
 	 */
-	
+
 	'use strict';
-	
-	
+
+
 	/**
 	 * Base Events Emitter implementation.
 	 *
@@ -567,7 +567,7 @@
 		if ( true ) {
 			if ( typeof this !== 'object' ) { throw new Error(__filename + ': must be constructed via new'); }
 		}
-	
+
 		/**
 		 * Inner hash table for event names and linked callbacks.
 		 * Manual editing should be avoided.
@@ -587,8 +587,8 @@
 		 **/
 		this.events = {};
 	}
-	
-	
+
+
 	Emitter.prototype = {
 		/**
 		 * Bind an event to the given callback function.
@@ -609,14 +609,14 @@
 				if ( typeof name !== 'string' || name.length === 0 ) { throw new Error(__filename + ': wrong or empty name'); }
 				if ( typeof callback !== 'function' ) { throw new Error(__filename + ': wrong callback type'); }
 			}
-	
+
 			// initialization may be required
 			this.events[name] = this.events[name] || [];
 			// append this new event to the list
 			this.events[name].push(callback);
 		},
-	
-	
+
+
 		/**
 		 * Add a one time listener for the event.
 		 * This listener is invoked only the next time the event is fired, after which it is removed.
@@ -627,13 +627,13 @@
 		once: function ( name, callback ) {
 			// current execution context
 			var self = this;
-	
+
 			if ( true ) {
 				if ( arguments.length !== 2 ) { throw new Error(__filename + ': wrong arguments number'); }
 				if ( typeof name !== 'string' || name.length === 0 ) { throw new Error(__filename + ': wrong or empty name'); }
 				if ( typeof callback !== 'function' ) { throw new Error(__filename + ': wrong callback type'); }
 			}
-	
+
 			// initialization may be required
 			this.events[name] = this.events[name] || [];
 			// append this new event to the list
@@ -642,8 +642,8 @@
 				self.removeListener(name, onceWrapper);
 			});
 		},
-	
-	
+
+
 		/**
 		 * Apply multiple listeners at once.
 		 *
@@ -655,13 +655,13 @@
 		 */
 		addListeners: function ( callbacks ) {
 			var name;
-	
+
 			if ( true ) {
 				if ( arguments.length !== 1 ) { throw new Error(__filename + ': wrong arguments number'); }
 				if ( typeof callbacks !== 'object' ) { throw new Error(__filename + ': wrong callbacks type'); }
 				if ( Object.keys(callbacks).length === 0 ) { throw new Error(__filename + ': no callbacks given'); }
 			}
-	
+
 			// valid input
 			if ( typeof callbacks === 'object' ) {
 				for ( name in callbacks ) {
@@ -671,8 +671,8 @@
 				}
 			}
 		},
-	
-	
+
+
 		/**
 		 * Remove all instances of the given callback.
 		 *
@@ -689,7 +689,7 @@
 				if ( typeof callback !== 'function' ) { throw new Error(__filename + ': wrong callback type'); }
 				if ( this.events[name] && !Array.isArray(this.events[name]) ) { throw new Error(__filename + ': corrupted inner data'); }
 			}
-	
+
 			// the event exists and should have some callbacks
 			if ( this.events[name] ) {
 				// rework the callback list to exclude the given one
@@ -701,8 +701,8 @@
 				}
 			}
 		},
-	
-	
+
+
 		/**
 		 * Remove all callbacks for the given event name.
 		 * Without event name clears all events.
@@ -717,7 +717,7 @@
 			if ( true ) {
 				if ( arguments.length !== 0 && (typeof name !== 'string' || name.length === 0) ) { throw new Error(__filename + ': wrong or empty name'); }
 			}
-	
+
 			// check input
 			if ( arguments.length === 0 ) {
 				// no arguments so remove everything
@@ -726,14 +726,14 @@
 				if ( true ) {
 					if ( this.events[name] ) { throw new Error(__filename + ': event is not removed'); }
 				}
-	
+
 				// only name is given so remove all callbacks for the given event
 				// but object structure modification should be avoided
 				this.events[name] = undefined;
 			}
 		},
-	
-	
+
+
 		/**
 		 * Execute each of the listeners in the given order with the supplied arguments.
 		 *
@@ -754,23 +754,23 @@
 		emit: function ( name, data ) {
 			var event = this.events[name],
 				i;
-	
+
 			if ( true ) {
 				if ( arguments.length < 1 ) { throw new Error(__filename + ': wrong arguments number'); }
 				if ( typeof name !== 'string' || name.length === 0 ) { throw new Error(__filename + ': wrong or empty name'); }
 			}
-	
+
 			// the event exists and should have some callbacks
 			if ( event ) {
 				if ( true ) {
 					if ( !Array.isArray(event) ) { throw new Error(__filename + ': wrong event type'); }
 				}
-	
+
 				for ( i = 0; i < event.length; i++ ) {
 					if ( true ) {
 						if ( typeof event[i] !== 'function' ) { throw new Error(__filename + ': wrong event callback type'); }
 					}
-	
+
 					// invoke the callback with parameters
 					// http://jsperf.com/function-calls-direct-vs-apply-vs-call-vs-bind/6
 					event[i].call(this, data);
@@ -778,20 +778,20 @@
 			}
 		}
 	};
-	
+
 	// correct constructor name
 	Emitter.prototype.constructor = Emitter;
-	
-	
+
+
 	if ( true ) {
 		// expose to the global scope
 		window.Emitter = Emitter;
 	}
-	
-	
+
+
 	// public
 	module.exports = Emitter;
-	
+
 	/* WEBPACK VAR INJECTION */}.call(exports, "/index.js"))
 
 /***/ }
